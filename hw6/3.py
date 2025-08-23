2cd38f6159f7deb13db341747c3089a625217bf5
Яна Осипенко <yanna92@yandex.ru>
2025-08-23 16:53:36 +0300

    Добавлена папка hw6 с решениями задач

---
+++ hw6/3.py
@@ -0,0 +1,5 @@
+degree_c = input("Введите температуру в градусах Цельсия:")
+degree_c = int (degree_c)
+degree_f = degree_c*9/5+32
+degree_f = float (degree_f)
+print("",degree_c,"°C это ",degree_f,"°F")
